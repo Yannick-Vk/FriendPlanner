e4c48695ba5c10dbc172b0969cbdf7a2c506cbe2
Yannick <vankerckvoordeyannick@gmail.com>
2026-03-27 15:20:52 +0100

    Fixed slider not reaching the end of the slider, made the slider center on the bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,9 +19,9 @@ const sliderValue = ref([40])
         <Slider.Track class="bg-gray-300 h-1 flex-grow rounded-full">
           <Slider.Range class="bg-blue-500 h-full rounded-full" />
         </Slider.Track>
-        <Slider.Thumb :index="0" class="w-[18px] h-[18px] bg-white rounded-full shadow border border-gray-400 cursor-pointer absolute  focus-visible:outline-none focus-visible:outline-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2" />
-
-
+        <Slider.Thumb :index="0" class="w-[18px] h-[18px] bg-white rounded-full shadow border
+        border-gray-400 cursor-pointer absolute  focus-visible:outline-none focus-visible:outline-blue-500
+        focus-visible:outline-2 focus-visible:outline-offset-2" />
       </Slider.Control>
     </Slider.Root>
   </div>
